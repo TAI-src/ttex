11bb875ec6c121046a7e4dd85e943d736df68ccf
Vanessa <3634850+CIGbalance@users.noreply.github.com>
2024-10-10 15:03:55 +0200

    Feat/release (#14)
    * adding cd
    
    * change name
    
    * temporarily add release now for PR
    
    * remove other release
    
    * adding permissions
    
    * more permissions
    
    * moving auth issues
    
    * adding correct name
    
    * correct name for release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name="ttex",
+    name="tai_ttex",
     version="0.1",
     packages=[
         "ttex",
@@ -9,4 +9,5 @@ setup(
     install_requires=[],
     license="GPL3",
     long_description="Tool for experiments",
+    long_description_content_type="text/x-rst",
 )

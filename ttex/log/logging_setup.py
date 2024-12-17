30252f9f1358b41d6a5cea9a9bef6f928069bdc2
Vanessa <3634850+CIGbalance@users.noreply.github.com>
2024-12-17 12:48:03 +0100

    more flexible logging setup (#29)
    * more flexible logging setup
    
    * explicit cast for mypy

--- ttex/log/logging_setup.py
+++ ttex/log/logging_setup.py
@@ -1,44 +1,55 @@
 """Convenient setup for python logging module"""
+
 import logging
 import logging.config
 import copy
-from typing import cast
+from typing import cast, Optional, Dict
 
-logging_config = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "formatters": {
-        "default": {
-            "format": "%(asctime)s - %(name)s - %(filename)s"
-            + " - %(funcName)s - %(threadName)s - %(levelname)s - %(message)s"
-        },
-    },
-    "handlers": {
-        "console": {
-            "level": "WARNING",
-            "class": "logging.StreamHandler",
-            "formatter": "default",
-        },
-        "root_console": {
-            "level": "WARNING",
-            "class": "logging.StreamHandler",
-            "formatter": "default",
-        },
-    },
-    "loggers": {
-        "DefaultLogger": {
-            "level": "WARNING",
-            "handlers": ["console"],
-        },
-    },
-    "root": {
-        "level": "WARNING",
-        "handlers": ["root_console"],
-    },
-}
+LOGGER_NAME = "DefaultLogger"
 
 
-def initiate_logger(log_level: int):
+def get_logging_config(logger_name: str, disable_existing: Optional[bool] = True):
+    config_dict = {
+        "version": 1,
+        "disable_existing_loggers": disable_existing,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s - %(name)s - %(filename)s"
+                + " - %(funcName)s - %(threadName)s - %(levelname)s - %(message)s"
+            },
+        },
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+            },
+            "root_console": {
+                "level": "WARNING",
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+            },
+        },
+        "loggers": {
+            logger_name: {
+                "level": "WARNING",
+                "handlers": ["console"],
+            },
+        },
+        "root": {
+            "level": "WARNING",
+            "handlers": ["root_console"],
+        },
+    }
+    return config_dict
+
+
+def initiate_logger(
+    log_level: int,
+    logger_name: Optional[str] = None,
+    disable_existing: Optional[bool] = True,
+    logging_config: Optional[Dict] = None,
+):
     """
     Convenience function to set the logging level of a given logger
 
@@ -51,17 +62,30 @@ def initiate_logger(log_level: int):
     If given log level is not recognised, set to default
     """
 
+    if not logger_name:
+        logger_name = LOGGER_NAME
+    if not logging_config:
+        logging_config = get_logging_config(logger_name, disable_existing)
     config = cast(dict, copy.deepcopy(logging_config))
+
+    assert logger_name in config["loggers"]
 
     chosen_level = logging.getLevelName(log_level)
     chosen_level_exists = chosen_level != f"Level {log_level}"
     if chosen_level_exists:  # Level exists - so change accordingly
-        config["loggers"]["DefaultLogger"]["level"] = chosen_level
-        config["handlers"]["console"]["level"] = chosen_level
+        config["loggers"][logger_name]["level"] = chosen_level
+        # TODO: potential debugging help for level issues
+        # handler_names = config["loggers"][logger_name]["handlers"]
+        # handler_levels = [
+        #    handler["level"]
+        #    for handler_name, handler in logging_config["handler"].items()
+        #    if handler_name in handler_names
+        # ]
 
     logging.config.dictConfig(config)
 
-    logger = logging.getLogger("DefaultLogger")
+    logger = logging.getLogger(logger_name)
+    logger.info(f"Set logging config to {config}")
     if chosen_level_exists:
         logger.debug(f"Set up log level for {log_level}")
     else:

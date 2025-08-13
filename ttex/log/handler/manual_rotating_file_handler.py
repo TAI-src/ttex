744ee3b4158cbc1c54cf83c6fa1a8e4895a585e0
TheHedgeify <thehedgeify@gmail.com>
2025-08-14 00:33:14 +0200

    file handler done

--- ttex/log/handler/manual_rotating_file_handler.py
+++ ttex/log/handler/manual_rotating_file_handler.py
@@ -1,49 +1,69 @@
-import logging
 from ttex.log.record import Header
 import os
+from logging.handlers import BaseRotatingHandler
 
-class ManualRotatingFileHandler(logging.handlers.BaseRotatingHandler):
+
+class ManualRotatingFileHandler(BaseRotatingHandler):
     """
     Custom RotatingFileHandler that allows manual rotation of log files.
     """
+
     def __init__(self, filepath, mode, encoding=None, errors=None):
         """
         Initialize the handler with the given filename and mode.
         """
         os.makedirs(os.path.dirname(filepath), exist_ok=True)
-        super().__init__(filepath, delay=True, mode=mode, encoding=encoding, errors=errors)
-        self.rotation_filepaths = []
-
+        super().__init__(
+            filepath, delay=True, mode=mode, encoding=encoding, errors=errors
+        )
+        self.current_filepath = None
+        self.next_filepath = None
 
     def shouldRollover(self, record):
         """
-        Determine if a rollover should occur, which is whenever a Header record is logged.
+        Determine if a rollover should occur,
+        which is whenever a Header record is logged with a new filepath
         """
-        should_rollover = False
-        if isinstance(record.msg, Header):
-            self.rotation_filepaths.append(record.msg.filepath) # Store the filepath for rotation
-            if len(self.rotation_filepaths) > 1:
-                # If this is not the first Header, we should rollover
-                should_rollover = True
-        assert len(self.rotation_filepaths) <= 2, "Only max 2 rotation filepath should be stored at a time."
-        assert len(self.rotation_filepaths) > 0, "No header record has been logged yet. Please log a Header record before emitting other records."
-        return should_rollover
+        if (
+            isinstance(record.msg, Header)
+            and record.msg.filepath != self.current_filepath
+        ):
+            if self.current_filepath is None:
+                # This is the first header file,
+                # set the current filepath to the first header's filepath
+                self.current_filepath = record.msg.filepath
+            else:
+                assert (
+                    self.next_filepath is None
+                ), "Next filepath should be None due to previous rollover."
+                self.next_filepath = record.msg.filepath
+        assert (
+            self.current_filepath is not None
+        ), "Current filepath should not be None. First message should always be a Header."
+        return self.next_filepath is not None
 
     def doRollover(self):
         """
         Perform the rollover by closing the current stream and renaming the file.
         """
-        filepath = self.rotation_filepaths.pop(0)
-        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        assert self.current_filepath is not None, "Current filepath should not be None."
+        print(self.current_filepath)
+        os.makedirs(os.path.dirname(self.current_filepath), exist_ok=True)
         if self.stream:
             self.stream.close()
-            self.stream = None
-        self.rotate(self.baseFilename, filepath)
+            self.stream = None  # type: ignore[assignment]
+        self.rotate(self.baseFilename, self.current_filepath)
+        self.current_filepath = self.next_filepath
+        self.next_filepath = None
         if not self.delay:
             self.stream = self._open()
 
     def close(self):
-        assert len(self.rotation_filepaths) == 1, "Handler should be closed with exactly one rotation filepath remaining."
-        self.doRollover()
+        assert (
+            self.next_filepath is None
+        ), "Next filepath should be None before closing."
+        if self.current_filepath is not None:
+            # Ensure we perform a rollover if there is a current filepath
+            # before closing the handler
+            self.doRollover()
         super().close()
- 

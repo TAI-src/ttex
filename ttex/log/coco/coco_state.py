f73fadc16e4b22cfb9c0bd2cc99c193447fbeac1
CIGbalance <3634850+CIGbalance@users.noreply.github.com>
2025-09-18 16:30:19 +0000

    :art: Format Python code with psf/black

--- ttex/log/coco/coco_state.py
+++ ttex/log/coco/coco_state.py
@@ -32,9 +32,9 @@ class COCOState(LoggingState):
         self.inst = coco_start.inst  # Problem instance number
         self.coco_start = coco_start  # Store the COCOStart event
         self.best_diff_opt: Optional[float] = None  # Best difference to optimal value
-        self.last_imp: Optional[float] = (
-            None  # Improvement of best_mf since last evaluation
-        )
+        self.last_imp: Optional[
+            float
+        ] = None  # Improvement of best_mf since last evaluation
         self._needs_start = False  # State now expects new COCOStart event
         self.last_tdat_emit = 0  # Last evaluation count when .tdat was emitted
         self.best_target = None  # Best target reached (from .dat logging)

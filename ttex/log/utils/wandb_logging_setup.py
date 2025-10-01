87910cb8b1f39f79a58094359e76b8dadee2432e
TheHedgeify <thehedgeify@gmail.com>
2025-10-01 11:42:18 +0200

    logging convenience setup

--- ttex/log/utils/wandb_logging_setup.py
+++ ttex/log/utils/wandb_logging_setup.py
@@ -2,11 +2,6 @@ import logging
 from ttex.log.handler import WandbHandler
 from typing import Optional, Dict, List
 import wandb
-from importlib.metadata import distributions
-from wandb.sdk import AlertLevel
-import os.path as osp
-from dataclasses import dataclass
-from ttex.log import capture_snapshot
 from ttex.log import LOGGER_NAME
 
 logger = logging.getLogger(LOGGER_NAME)
@@ -14,6 +9,8 @@ logger = logging.getLogger(LOGGER_NAME)
 
 def setup_wandb_logger(
     custom_metrics: Optional[Dict] = None,
+    snapshot: bool = True,
+    snapshot_sensitive_keys: Optional[List[str]] = None,
     name: str = "wandb_logger",
     level: int = logging.INFO,
 ) -> logging.Logger:
@@ -24,7 +21,12 @@ def setup_wandb_logger(
         wandb_logger.propagate = False  # Prevent double logging
         wandb_logger.setLevel(level)
 
-        wandb_handler = WandbHandler(custom_metrics=custom_metrics)
+        wandb_handler = WandbHandler(
+            custom_metrics=custom_metrics,
+            snapshot=snapshot,
+            snapshot_sensitive_keys=snapshot_sensitive_keys,
+            level=level,
+        )
         wandb_handler.setLevel(level)
         wandb_logger.addHandler(wandb_handler)
     return wandb_logger
@@ -38,16 +40,30 @@ def teardown_wandb_logger(name: str = "wandb_logger") -> None:
     wandb_logger._wandb_setup = False  # type: ignore[attr-defined]
 
 
-def get_wandb_logger(name: str = "wandb_logger") -> Optional[logging.Logger]:
+def _get_wandb_logger(name: str = "wandb_logger") -> Optional[logging.Logger]:
     wandb_logger = logging.getLogger(name)
     if not getattr(wandb_logger, "_wandb_setup", None):
         return None
+
     else:
         return wandb_logger
 
 
-def get_wandb_handler(name: str = "wandb_logger") -> Optional[WandbHandler]:
-    wandb_logger = get_wandb_logger(name=name)
+def get_wandb_logger(name: str = "wandb_logger") -> Optional[logging.Logger]:
+    """
+    Get the wandb logger if it exists and is properly set up with a wandb run
+    """
+    wandb_logger = _get_wandb_logger(name=name)
+    if wandb_logger is None:
+        return None
+    wandb_handler = _get_wandb_handler(name=name)
+    if wandb_handler is None or not getattr(wandb_handler, "run", None):
+        return None
+    return wandb_logger
+
+
+def _get_wandb_handler(name: str = "wandb_logger") -> Optional[WandbHandler]:
+    wandb_logger = _get_wandb_logger(name=name)
     if wandb_logger is None:
         return None
     wandb_handler = next(
@@ -62,7 +78,7 @@ def log_wandb_init(
     group: Optional[str] = None,
     logger_name: str = "wandb_logger",
 ) -> wandb.sdk.wandb_run.Run:
-    handler = get_wandb_handler(name=logger_name)
+    handler = _get_wandb_handler(name=logger_name)
     if handler is None:
         logger.warning("WandbHandler not found")
         return None
@@ -77,12 +93,13 @@ def log_wandb_artifact(
     local_path: str,
     artifact_type: str = "evaluation",
     description: str = "",
-) -> None:
-    handler = get_wandb_handler(name=logger_name)
+) -> Optional[wandb.Artifact]:
+    handler = _get_wandb_handler(name=logger_name)
     if handler is None or not getattr(handler, "run", None):
         logger.warning("WandbHandler not found or not initialized with wandb run")
         return
-    return handler.create_wandb_artifact(
+    return WandbHandler.create_wandb_artifact(
+        run=handler.run,
         artifact_name=artifact_name,
         local_path=local_path,
         artifact_type=artifact_type,
